bd5508d9d8ab20aca71b4f95167e42ef09541881
simpart <simpart>
2018-09-09 14:02:19 +0000

    add execont

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
  * @file mofron-effect-vrtpos/index.js
  * @author simpart
  */
-
+const mf = require('mofron');
 /**
  * @class VrtPos
  * @brief Vertical Position effect class
  */
 mofron.effect.VrtPos = class extends mofron.Effect {
     
-    constructor (po) {
+    constructor (po, p2) {
         try {
             super();
             this.name('VrtPos');
-            this.prmMap('type');
-            this.prmOpt(po);
+            this.prmMap('type', 'offset');
+            this.prmOpt(po, p2);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -23,54 +23,69 @@ mofron.effect.VrtPos = class extends mofron.Effect {
     
     enable (cmp) {
         try {
-            if ('center' === this.type()) {
-                if (true === mofron.func.isInclude(cmp, 'Text')) {
-                    if ((null !== cmp.target().parent()) && ('flex' === cmp.target().parent().style('display'))) {
-                        cmp.target().parent().style({
-                            'align-items' : 'center'
-                        });
-                    } else {
-                        cmp.style({
-                            'margin-top'    : 'auto',
-                            'margin-bottom' : 'auto',
-                        });
-                    }
-                } else {
-                    cmp.style({
-                        'position' : 'relative',
-                        'top'      : '50%'     ,
-                        '-webkit-transform' : 'translateY(-50%)',
-                        'transform'         : 'translateY(-50%)'
-                    });
-                }
-            }
+            this.execonts(cmp, true);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    disable (tgt) {
+    disable (cmp) {
+        try {
+            this.execonts(cmp, false);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    execonts (cmp, flg) {
         try {
             if ('center' === this.type()) {
                 if (true === mofron.func.isInclude(cmp, 'Text')) {
-                    if ((null !== cmp.parent()) && ('flex' === cmp.style('display'))) {
-                        cmp.style({
-                            'align-items' : null
+                    if ( (null   !== cmp.target().parent()) &&
+                         ('flex' === cmp.target().parent().style('display')) ) {
+                        cmp.target().parent().style({
+                            'align-items' : (true === flg) ? 'center' : null
                         });
+                    } else if ( (null   !== cmp.target().parent()) &&
+                                ('absolute' === cmp.target().parent().style('position')) ) {
+                        cmp.style({
+                            'position' : (true === flg) ? 'relative' : null,
+                            'top'      : (true === flg) ? '50%' : null,
+                            '-webkit-transform' : (true === flg) ? 'translateY(-50%)' : null,
+                            'transform'         : (true === flg) ? 'translateY(-50%)' : null
+                        });
+                        if ((true === flg) && (null !== this.offset())) {
+                            cmp.sizeValue(
+                                'top',
+                                mf.func.sizeSum(cmp.sizeValue('top'), this.offset())
+                            );
+                        }
                     } else {
                         cmp.style({
-                            'margin-top'    : null,
-                            'margin-bottom' : null,
+                            'margin-top'    : (true === flg) ? 'auto' : null,
+                            'margin-bottom' : (true === flg) ? 'auto' : null
                         });
                     }
                 } else {
                     cmp.style({
-                        'position' : null,
-                        'top'      : null     ,
-                        '-webkit-transform' : null,
-                        'transform'         : null
+                        'position' : (true === flg) ? 'relative' : null,
+                        'top'      : (true === flg) ? '50%'      : null,
+                        '-webkit-transform' : (true === flg) ? 'translateY(-50%)' : null,
+                        'transform'         : (true === flg) ? 'translateY(-50%)' : null
                     });
+                }
+            } else if ('bottom' === this.type()) {
+                if ( (null   !== cmp.target().parent()) &&
+                     ('absolute' === cmp.target().parent().style('position')) ) {
+                    cmp.style({
+                        'position' : (true === flg) ? 'absolute' : null,
+                        'bottom'   : (true === flg) ? '0%'       : null
+                    });
+                    if ((true === flg) && (null !== this.offset())) {
+                        cmp.style({'bottom' : this.offset()});
+                    }
                 }
             }
         } catch (e) {
@@ -89,12 +104,29 @@ mofron.effect.VrtPos = class extends mofron.Effect {
             if ('string' !== typeof prm) {
                 throw new Error('invalid parameter');
             }
-            if ( ('left'   !== prm) &&
+            if ( ('top'    !== prm) &&
                  ('center' !== prm) &&
-                 ('right'  !== prm) ) {
+                 ('bottom' !== prm) ) {
                 throw new Error('invalid parameter');
             }
             this.m_type = prm;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    offset (prm) {
+        try {
+            if (undefined === prm) {
+                /* getter */
+                return (undefined === this.m_offset) ? null : this.m_offset;
+            }
+            /* setter */
+            if ('string' !== typeof prm) {
+                throw new Error('invalid parameter');
+            }
+            this.m_offset = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
