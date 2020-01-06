6ffa287548e671dd1aee1618180a69223e54392e
simpart <simpart>
2020-01-07 08:42:08 +0900

    supported v1.1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # mofron-effect-vrtpos
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
 vertical position effect for mofron component
+
+the component is positioned specified parameter that is 'center' or 'top' and 'bottom'.
+
+
+# Install
+```
+npm install mofron mofron-effect-vrtpos
+```
+
+# Sample
+```html
+<require>
+    <tag load="mofron-effect-vrtpos">VrtPos</tag>
+</require>
+<div size=(100%,0.5rem) style="display:flex;">
+    <Text effect=VrtPos("top")>Vertical Position</Text>
+</div>
+```
+
+# Parameter
+
+| Short<br>Form | Parameter Name | Type | Description |
+|:-------------:|:---------------|:-----|:------------|
+| ◯  | type | string | position type (center,top,bottom) |
+| ◯  | offset | string | offset size (css value) |
+
--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 /**
  * @file mofron-effect-vrtpos/index.js
  * @brief vertical position effect for mofron component
- * @author simpart
+ *        the component is positioned specified parameter that is 'center' or 'top' and 'bottom'.
+ * @license MIT
  */
-const mf = require('mofron');
-const transform = require('mofron-transform');
+require('mofron-util-transform');
+const comutl = mofron.util.common;
 
-mofron.effect.VrtPos = class extends mofron.Effect {
+
+module.exports = class extends mofron.class.Effect {
     /**
      * initialize vertical position effect
      *
-     * @param p1 (object) effect option
-     * @param p1 (string) position type
-     * @param p2 (string) offset size
+     * @param (mixed) type parameter
+     *                key-value: effect config
+     * @param offset parameter
+     * @short type,offset
+     * @type private
      */
-    constructor (po, p2) {
+    constructor (p1, p2) {
         try {
             super();
-            this.name('VrtPos');
-            this.prmMap(['type', 'offset']);
-            this.valType('%');
-            this.prmOpt(po, p2);
+            this.name("VrtPos");
+            this.shortForm("type","offset");
+            
+            this.confmng().add("type", { type: "string", select: ["top", "center", "bottom"], init: "center" });
+            this.confmng().add("offset", { type: "size" });
+
+	    if (0 < arguments.length) {
+                this.config(p1,p2);
+	    }
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -28,38 +37,25 @@ mofron.effect.VrtPos = class extends mofron.Effect {
     }
     
     /**
-     * vertical position effect
+     * effet contents
      *
-     * @note private method
+     * @type private
      */
     contents (cmp) {
         try {
-            if (null !== this.contsIndex()) {
-                this.contsList(this.contsIndex())(this, cmp);
-                return;
-            }
-            let flg = this.valid();
-            if (true === mofron.func.isInclude(cmp, 'Text')) {
-                this.contsTxt(cmp, flg);
-                return;
-            }
-            if ('center' === this.type()) {
-                if ( ("absolute" === cmp.style("position")) ||
-                     ("relative" === cmp.style("position")) ) {
-                    this.contsList(1)(this, cmp);
-                } else if ("flex" === cmp.target().parent().style("display")) {
-                    this.contsList(0)(this, cmp);
-                } else {
-                    console.warn("not supported component style");
-                }
-            } else {
-                if ( ("absolute" === cmp.style("position")) ||
-                     ("fixed" === cmp.style("position")) ) {
-                    this.contsList(2)(this, cmp);
-                } else {
-                    this.contsList(0)(this, cmp);
-                }
-            }
+            if (true === comutl.isinc(cmp,"Text")) {
+                this.txtpos(cmp);
+		return;
+	    } else if ( ("absolute" === cmp.style("position")) ||
+	                ("relative" === cmp.style("position")) ||
+			("fixed" === cmp.style("position")) ) {
+                this.toppos(cmp);
+	    } else if ("flex" === cmp.childDom().parent().style("display")) {
+                this.mgnpos(cmp);
+	    } else {
+	        cmp.style({ "position" : "relative" }, { passive: true });
+                this.toppos(cmp);
+	    }
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -67,141 +63,106 @@ mofron.effect.VrtPos = class extends mofron.Effect {
     }
     
     /**
-     * vertical position effect for text component
+     * text component position
+     * 
+     * @param (component) target component
+     * @type private
      */
-    contsTxt (cmp, flg) {
+    txtpos (cmp) {
         try {
-            if ('center' === this.type()) {
-                if ( (null   !== cmp.target().parent()) &&
-                     ('flex' === cmp.target().parent().style('display')) ) {
-                    cmp.target().parent().style({
-                        'align-items' : (true === flg) ? this.type() : null
-                    });
-                } else if ( ("absolute" === cmp.style("position")) ||
-                            ("relative" === cmp.style("position")) ) {
-                    this.contsList(1)(this, cmp);
-                } else if ( ("grid" === cmp.target().parent().style('display')) ||
-                            ("inline-grid" === cmp.target().parent().style('display')) ) {
-                    this.contsList(0)(this, cmp);
-                } else if ( (null   !== cmp.target().parent()) && 
-		            (cmp.getId() !== cmp.parent().getChild(true)[0].getId()) &&
-		            (('relative' === cmp.target().parent().style('position')) ||
-			     ('fixed' === cmp.target().parent().style('position'))) ) {
-		    cmp.style({
-		        "position"  : "absolute",
-                        "top"       : "0rem",
-		    });
-		    transform.translate(cmp, undefined, "50%");
-		} else {
-                    console.warn("not supported component style");
-                }
+	    let off = this.offset();
+	    if ( (null !== cmp.childDom().parent()) &&
+	         ('flex' === cmp.childDom().parent().style('display')) ) {
+                let ai = "center";
+                if ("center" !== this.type()) {
+		    ai = ("top" === this.type()) ? "flex-start" : "flex-end";
+		}
+                cmp.childDom().parent().style({ "align-items" : ai });
+		if (null !== off) {
+                    cmp.style({ "position" : "relative" }, { passive: true });
+		    cmp.style({ "top" : off.toString() });
+		}
             } else {
-                if ( ("relative" === cmp.style("position")) ||
-                     ("absolute" === cmp.style("position")) ) {
-                    this.contsList(2)(this, cmp);
-                } else {
-                    this.contsList(0)(this, cmp);
-                }
-            }
+                cmp.style({ "position" : "relative" }, { passive: true });
+                this.toppos(cmp);
+	    }
         } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    contsIndex (prm) {
-        try { return this.member('contsIndex', 'number', prm); } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 
-    contsList (idx) {
+    /**
+     * set position by top value
+     * 
+     * @param (component) target component
+     * @type private
+     */
+    toppos (cmp) {
         try {
-            let conts = [
-                (eff, cmp) => {
-                    try {
-                        if ('center' === eff.type()) {
-                            cmp.style({
-                                'margin-top': (true === eff.valid()) ? 'auto' : null,
-                                'margin-bottom' : (true === eff.valid()) ? 'auto' : null
-                            });
-                            if (null !== eff.offset()) {
-                                cmp.style({ 'position': 'relative', 'top': eff.offset() });
-                            }
-                        } else if ('top' === eff.type()) {
-                            cmp.style({
-                                'margin-top': (true === eff.valid()) ? eff.offset() : null,
-                                'margin-bottom' : (true === eff.valid()) ? 'auto' : null
-                            });
-                        } else {
-                            cmp.style({
-                                'margin-top': (true === eff.valid()) ? 'auto' : null,
-                                'margin-bottom' : (true === eff.valid()) ? eff.offset() : null,
-                            });
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-                (eff, cmp) => {
-                    try {
-                        if ('center' === eff.type()) {
-                            cmp.style({
-                                'top' : (true === eff.valid()) ? eff.getValue('50%') : null,
-                            });
-			    transform.translate(cmp, undefined, "-50%");
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-                (eff, cmp) => {
-                    try {
-                        let val = (null !== this.offset()) ? this.offset() : '0rem';
-                        if ('top' === eff.type()) {
-                            cmp.style({
-                                //'position' : (true === eff.valid()) ? 'absolute' : null,
-                                'top'      : (true === eff.valid()) ? val : null
-                            });
-                        } else if ('bottom' === eff.type()) {
-                            cmp.style({
-                                //'position' : (true === eff.valid()) ? 'absolute' : null,
-                                'bottom'   : (true === eff.valid()) ? val : null
-                            });
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-            ];
-            return conts[idx];
-        } catch (e) {
+	    let off = this.offset();
+            if ("center" === this.type()) {
+                cmp.style({ "top" : "50%" });
+                cmputl.translate(cmp, undefined, "-50%");
+                if (null !== off) {
+                    cmp.style({ "margin-top" : off.toString() });
+		}
+	    } else if ("top" === this.type()) {
+                cmp.style({ "top" : (null !== off) ? off.toString() : "0rem" });
+	    } else {
+                cmp.style({ "bottom" : (null !== off) ? off.toString() : "0rem" });
+	    }
+	} catch (e) {
             console.error(e.stack);
             throw e;
-        }
-    }
-    
-    valid (prm) {
-        try { return this.member('valid', 'boolean', prm, true); } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
+	}
     }
     
     /**
-     * position type setter/getter
+     * set position by margin value
+     * 
+     * @param (component) target component
+     * @type private
+     */
+    mgnpos (cmp) {
+        try {
+            let off = this.offset();
+            if ("center" === this.type()) {
+                cmp.style({
+                    "margin-top"    : "auto",
+		    "margin-bottom" : "auto"
+                });
+		if (null !== off) {
+                    cmp.style({ "position" : "relative" }, { passive: true });
+                    cmp.style({ "top" : off.toString() });
+                }
+            } else if ("top" === this.type()) {
+                cmp.style({
+                    "margin-top"    : (null !== off) ? off.toString() : "0rem",
+                    "margin-bottom" : "auto"
+                }); 
+	    } else {
+                cmp.style({
+                    "margin-top"    : "auto",
+		    "margin-bottom" : (null !== off) ? off.toString() : "0rem"
+	        });
+            }
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
+    }
+    
+    /**
+     * position type
      *
-     * @param p1 (string) position type
-     * @param p1 (undefined) call as getter
+     * @param (string) position type (center,top,bottom)
      * @return (string) position type
+     * @type parameter
      */
     type (prm) {
         try {
-            return this.member('type', ['top', 'center', 'bottom'], prm, 'center');
+	    return this.confmng("type", prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -209,56 +170,19 @@ mofron.effect.VrtPos = class extends mofron.Effect {
     }
     
     /**
-     * offset size setter/getter
+     * offset size
      *
-     * @param p1 (string) offset size (css value)
-     * @param p1 (undefined) call as getter
-     * @return (string) offset size (css value)
+     * @param (string) offset size (css value)
+     * @return (mofron.class.Size) offset size
+     * @type parameter
      */
     offset (prm) {
         try {
-            return this.member(
-                'offset',
-                'string', 
-                (undefined !== prm) ? mf.func.getSize(prm).toString() : prm,
-                null
-            ); 
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    valType (prm) {
-        try { return this.member('valType', 'string', prm, 'rem'); } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    getValue (prm) {
-        try {
-            let val = '0' + this.valType();
-            if (undefined !== prm) {
-                val = mf.func.getSize(prm);
-                if (null === val) {
-                    throw new Error('invalid paramter');
-                }
-                this.valType(val.type());
-            }
-
-            if (null !== this.offset()) {
-                try { return mf.func.sizeSum(val, this.offset()); } catch (e) {
-                    return val;
-                }
-            } else {
-                return val;
-            }
+	    return this.confmng("offset", prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 }
-module.exports = mofron.effect.VrtPos;
 /* end of file */
