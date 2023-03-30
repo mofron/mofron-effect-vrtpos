b0f1c9c9ee58da911c477e4da74104f08ad16c8e
EC2 Default User <ec2-user@ip-172-26-5-87.ap-northeast-1.compute.internal>
2023-03-30 14:24:49 +0000

    mod parameter

--- index.js
+++ index.js
@@ -22,11 +22,13 @@ module.exports = class extends mofron.class.Effect {
         try {
             super();
             this.modname("VrtPos");
-            this.shortForm("type","offset");
+            this.shortForm("position","offset");
 	    
             /* init config */
-            this.confmng().add("type", { type: "string", select: ["top", "center", "bottom"], init: "center" });
+            this.confmng().add("position", { type: "string", select: ["top", "center", "bottom"], init: "center" });
             this.confmng().add("offset", { type: "size" });
+            this.confmng().add('style',    { type:'string', init:'auto', select:['auto','margin','transform'] });
+            this.confmng().add('target',   { type:'Dom' });
             
             /* set config */
 	    this.innerTgt(false);
@@ -53,21 +55,37 @@ module.exports = class extends mofron.class.Effect {
                 if (true === comutl.isinc(cmp,"Text")) {
                     this.txtpos(rdom[ridx]);
 		    return;
-	        } else if ( ("absolute" === rdom[ridx].style("position")) ||
-	                    ("relative" === rdom[ridx].style("position")) ||
-	                    ("fixed" === rdom[ridx].style("position")) ) {
-                    this.toppos(rdom[ridx]);
-	        } else if ( ("flex" === rdom[ridx].parent().style("display")) ||
-		            ("flex" === cmputl.dispbuff(rdom[ridx])) ) {
-                    this.mgnpos(rdom[ridx]);
-	        } else {
-	            rdom[ridx].style({ "position" : "relative" }, { passive: true });
-                    this.toppos(rdom[ridx]);
 	        }
 
-		if ("flex" !== rdom[ridx].parent().style("display")) {
-                    break;
-		}
+                if ("margin" === this.style()) {
+                    if (null !== this.target()) {
+                        this.mgnpos(this.target());
+                        break;
+                    }
+                    this.mgnpos(rdom[ridx]);
+                } else if ("transform" === this.style()) {
+                    if (null !== this.target()) {
+                        this.toppos(this.target());
+                        break;
+                    }
+                    this.toppos(rdom[ridx]);
+                } else {
+		    if ( ("absolute" === rdom[ridx].style("position")) ||
+	                        ("relative" === rdom[ridx].style("position")) ||
+	                        ("fixed" === rdom[ridx].style("position")) ) {
+                        this.toppos(rdom[ridx]);
+	            } else if ( ("flex" === rdom[ridx].parent().style("display")) ||
+		                ("flex" === cmputl.dispbuff(rdom[ridx])) ) {
+                        this.mgnpos(rdom[ridx]);
+	            } else {
+	                rdom[ridx].style({ "position" : "relative" }, { passive: true });
+                        this.toppos(rdom[ridx]);
+	            }
+                    
+		    if ("flex" !== rdom[ridx].parent().style("display")) {
+                        break;
+		    }
+                }
 	    }
         } catch (e) {
             console.error(e.stack);
@@ -88,9 +106,9 @@ module.exports = class extends mofron.class.Effect {
 	    if (null !== pnt) {
                 let pnt_disp = ("none" === pnt.style("display")) ? cmputl.dispbuff(pnt) : pnt.style("display");
                 if (("flex" === pnt_disp) || ('grid' === pnt_disp)) {
-		    if ("center" === this.type()) {
+		    if ("center" === this.position()) {
 		        pnt.style({ "align-items" : "center" });
-                    } else if ("bottom" === this.type()) {
+                    } else if ("bottom" === this.position()) {
                         pnt.style({ "align-items" : "flex-end" });
 		    }
 		    if (null !== off) {
@@ -117,13 +135,16 @@ module.exports = class extends mofron.class.Effect {
     toppos (dom) {
         try {
 	    let off = this.offset();
-            if ("center" === this.type()) {
+	    if (null === dom.style('position')) {
+                dom.style({ 'position':'relative' });
+	    }
+            if ("center" === this.position()) {
 	        dom.style({ "top" : "50%" });
 		transfm(dom, undefined, "-50%");
                 if (null !== off) {
 		    dom.style({ "margin-top" : off });
 		}
-	    } else if ("top" === this.type()) {
+	    } else if ("top" === this.position()) {
 	        dom.style({ "top" : (null !== off) ? off : "0rem" });
 	    } else {
 	        dom.style({ "bottom" : (null !== off) ? off : "0rem" });
@@ -143,13 +164,13 @@ module.exports = class extends mofron.class.Effect {
     mgnpos (dom) {
         try {
             let off = this.offset();
-            if ("center" === this.type()) {
+            if ("center" === this.position()) {
 	        dom.style({ "margin-top": "auto", "margin-bottom": "auto" });
 		if (null !== off) {
 		    dom.style({ "position" : "relative" }, { passive: true });
 		    dom.style({ "top" : off });
                 }
-            } else if ("top" === this.type()) {
+            } else if ("top" === this.position()) {
 	        dom.style({
                     "margin-top"    : (null !== off) ? off : "0rem",
 		    "margin-bottom" : "auto"
@@ -174,9 +195,9 @@ module.exports = class extends mofron.class.Effect {
      * @return (string) position type
      * @type parameter
      */
-    type (prm) {
+    position (prm) {
         try {
-	    return this.confmng("type", prm);
+	    return this.confmng("position", prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -194,6 +215,24 @@ module.exports = class extends mofron.class.Effect {
     offset (prm) {
         try {
 	    return this.confmng("offset", prm);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    style (prm) {
+        try {
+            return this.confmng('style', prm);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    target (prm) {
+        try {
+            return this.confmng('target', prm);
         } catch (e) {
             console.error(e.stack);
             throw e;
